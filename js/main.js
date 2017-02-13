8927ee2fb72d00857ccbad735cfeca6455f3c9c2
Jordan Hoeber <jrhoeber@buffalo.edu>
2017-02-13 14:09:46 -0500

    less-rough draft

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 $( document ).ready(function() {
-
 });
-
+$(function(){
+  // See if this is a touch device
+  if ('ontouchstart' in window)
+  {
+    // Set the correct body class
+    $('body').removeClass('no-touch').addClass('touch');
+    
+    // Add the touch toggle to show text
+    $('div.boxInner img').click(function(){
+      $(this).closest('.boxInner').toggleClass('touchFocus');
+    });
+  }
+});
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
